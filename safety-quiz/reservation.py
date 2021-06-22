95d0539b1083551974c5128287db5db5dfcb562d
alirpJ <jprila@hawk.iit.edu>
2021-06-22 10:56:23 -0500

    Reservations moved

--- safety-quiz/reservation.py
+++ safety-quiz/reservation.py
@@ -2,8 +2,18 @@ import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 _base_reservation = declarative_base()
-
 from typing import Union, Callable, Tuple, Optional
+import datetime
+from flask import request, session, redirect, url_for, render_template, flash, jsonify, g, Blueprint
+import sqlalchemy as sa
+
+from checkIn.model import User, UserLocation, Type, Training, Machine, Quiz, Question, Option, MissedQuestion, init_db, Major, College, HawkCard
+from reservation import ReservationType, ReservationWindow, Reservations, HasRemoveMethod, init_reservation_db
+
+reservation = Blueprint('reservation', __name__)
+
+db_session = init_db(reservation.config['DB'])
+db_reservations = init_reservation_db(reservation.config['DB_RESERVATION'])
 
 # Just for type-hinting, if you know a better way please fix
 class HasRemoveMethod:
@@ -53,3 +63,151 @@ class Reservations(_base_reservation):
 
     def __repr__(self):
         return "%s has a %s reservation from %s to %s" % (self.sid, self.type.name, self.start, self.end)
+
+
+@reservation.route('/reservations', methods=['GET','POST']) # KEEP THIS
+def reservations():
+    if request.method == 'GET':
+        ##temp disable reservations since users can still reserve time for current day even if in disbaled range
+        if reservation.config['ALLOW_RESERVATIONS'] != 'True':
+            flash("Reservations are currently unavailable and will resume for the Spring 2021 semester.",'warning')
+            return render_template('layout.html')
+        db = g.db_session()
+        temp = db.query(UserLocation).filter_by(sid=session['sid']).filter_by(location_id=2).one_or_none().get_missing_trainings(db)
+        if (9 in [each[0].id for each in temp]):
+            flash("You are not cleared to make reservations at this time. Please chcek the status of your safety trainings or contact Idea Shop staff for assistance.",'warning')
+            return render_template('layout.html')
+        user = db.query(User).filter_by(sid=session['sid']).one_or_none()
+        reservation_types = db_reservations().query(ReservationType).order_by(ReservationType.id.asc()).all()
+        db.close()
+        return render_template('reservations.html', reservation_types=reservation_types, user=user, openDate=datetime.date(2020, 9, 8), dateWindows=get_window())
+    if request.method == 'POST':
+        user = db_session().query(User).filter_by(sid=session['sid']).one_or_none()
+        start_time = datetime.datetime.strptime(request.form['start_time'],"%Y-%m-%d %X")
+        end_time = datetime.datetime.strptime(request.form['end_time'],"%Y-%m-%d %X")
+        db = db_reservations()
+        reservation_type = db.query(ReservationType).filter_by(id=int(request.form['reservation_type'])).one_or_none()
+        parent = Reservations(type_id=reservation_type.id, start=start_time, end=end_time, sid=user.sid)
+        db.add(parent)
+        db.flush()
+        flash("Created %s reservation for %s, %s - %s" % (reservation_type.name, user.name, parent.start, parent.end),'success')
+        for each in [x for x in list(request.form.keys()) if 'user' in x and request.form[x] != '']:
+            response = confirmAllowed(request.form[each])
+            if response['valid'] is True:
+                db.add(Reservations(type_id=int(request.form['reservation_type']), start=start_time, end=end_time, sid=response['user'].sid, parent_id=parent.id))
+                db.flush()
+                flash("Created %s reservation for %s, %s - %s" % (reservation_type.name, response['user'].name, parent.start, parent.end),'success')
+        db.commit()
+        return redirect(url_for('reservations'))
+
+def confirmAllowed(email):
+    db = g.db_session()
+    user = db.query(User).filter_by(email=email).one_or_none()
+    if user:
+        userLocation = db.query(UserLocation).filter_by(sid=user.sid).filter_by(location_id=2).one_or_none()
+        if userLocation :
+            temp = userLocation.get_missing_trainings(db)
+            if (9 in [each[0].id for each in temp]):
+                flash("User %s is not cleared for reservations." % email, 'danger')
+                return {'valid': False,'user': user}
+            else:
+                return {'valid': True, 'user': user}
+    else:
+        flash("User %s does not exist and cannot join reservations." % email, 'danger')
+        return {'valid': False, 'user': None}
+
+@reservation.route('/reservations/api/checkEmail')
+def checkEmail():
+    db = g.db_session()
+    user = db.query(User).filter_by(email=request.args['email']).one_or_none()
+    if user:
+        userLocation = db.query(UserLocation).filter_by(sid=user.sid).filter_by(location_id=2).one_or_none()
+        if userLocation:
+            temp = userLocation.get_missing_trainings(db)
+            if (9 in [each[0].id for each in temp]):
+                return jsonify({'valid': False, 'reason': "User not cleared for reservations."})
+            else:
+                return jsonify({'valid': True, 'reason': "User cleared for reservations."})
+        else:
+            return jsonify({'valid': False, 'reason': "User not cleared for reservations."})
+    return jsonify({'valid': False, 'reason': "User does not exist."})
+
+@reservation.route('/reservations/api/type')
+def get_type():
+    x = g.db_reservations().query(ReservationType).filter_by(id=int(request.args['type_id'])).one_or_none()
+    if x :
+        return jsonify({'id':x.id,'name':x.name,'quantity':x.quantity,'capacity':x.capacity})
+    else:
+        return ''''''
+
+@reservation.route('/reservations/api/start_times', methods=['GET'])
+def start_times():
+    db = g.db_reservations()
+    requested_date = datetime.datetime.strptime(request.args['date'][:15], "%a %b %d %Y").date()
+    reservation_type = db.query(ReservationType).filter_by(id=int(request.args['type_id'])).one()
+    existing_reservations = db.query(Reservations).filter(Reservations.start >= requested_date).filter(Reservations.end < requested_date+datetime.timedelta(days=1)).filter(Reservations.type_id == reservation_type.id).filter(Reservations.parent_id == None).all()
+    open = True
+    if open:
+        open_time = datetime.datetime.combine(requested_date,datetime.time(9,0))
+        close_time = datetime.datetime.combine(requested_date,datetime.time(17,0))
+        delta_time = datetime.timedelta(hours=0,minutes=15)
+        start_times = []
+        for i in range(open_time.hour*60+open_time.minute,close_time.hour*60+close_time.minute,int(delta_time.seconds / 60)):
+            i = datetime.datetime.combine(requested_date,datetime.time(int(i/60),int(i%60)))
+            availability = reservation_type.quantity
+            for each in existing_reservations:
+                if i > (each.start - (2*delta_time)) and i < (each.end + (2*delta_time)):
+                    availability -= 1
+            if availability > 0:
+                start_times.append(i)
+        return jsonify([{'date': str(x.date()), 'time': str(x.time())} for x in start_times])
+
+@reservation.route('/reservations/api/end_times', methods=['GET'])
+def end_times():
+    db = db_reservations()
+    reservation_type = db.query(ReservationType).filter_by(id = int(request.args['type_id'])).one()
+    start_time = datetime.datetime.strptime(request.args['start_time'],"%Y-%m-%d %X")
+    existing_reservations = db.query(Reservations).filter(Reservations.start >= start_time).filter(Reservations.end < start_time.date() + datetime.timedelta(days=1)).filter(Reservations.type_id == reservation_type.id).filter(Reservations.parent_id == None).all()
+    close_time = datetime.datetime.combine(start_time.date(), datetime.time(17, 0))
+    delta_time = datetime.timedelta(hours=0, minutes=15)
+    max_reservation_minutes = 180
+    end_times = []
+    for i in range(((start_time+delta_time).hour * 60) + (start_time+delta_time).minute, min((close_time.hour * 60) + close_time.minute,((start_time+delta_time).hour * 60) + (start_time+delta_time).minute + max_reservation_minutes), int(delta_time.seconds / 60)):
+        i = datetime.datetime.combine(start_time.date(), datetime.time(int(i / 60), int(i % 60)))
+        availability = reservation_type.quantity
+        for each in existing_reservations:
+            if i > (each.start - (2*delta_time)):
+                availability -= 1
+        if availability > 0:
+            end_times.append(i)
+        else: break
+    return jsonify([{'date': str(x.date()), 'time': str(x.time())} for x in end_times])
+
+@reservation.route('/reservations/api/windows', methods=['GET'])
+def get_window():
+    db = db_reservations()
+    temp = db.query(ReservationWindow).filter(ReservationWindow.start >= datetime.datetime.today().date()).all()
+    return [x.start.strftime('%m %d %Y') for x in temp]
+
+@reservation.route('/reservations/view')
+def view_reservations():
+    db = db_reservations()
+    start = datetime.datetime.combine(datetime.datetime.now().date(),datetime.time(0,0,0))
+    end = datetime.datetime.combine(datetime.datetime.now().date(),datetime.time(23,59,59))
+    reservations = db.query(Reservations).filter(Reservations.start > start).filter(Reservations.end < end).order_by(Reservations.start.asc()).all()
+    return render_template('view_reservations.html', reservations=reservations)
+
+@reservation.route('/api/machine_access', methods=['POST'])
+def get_machine_access():
+    if not request.form or not all(items in request.form.keys() for items in ['machine_name', 'machine_id']):
+        return jsonify({'response': 'Invalid request.'})
+    machine_id = int(request.form['machine_id'])
+    machine_name = request.form['machine_name']
+    db = db_session()
+    machine = db.query(Machine).filter_by(id=machine_id).one_or_none()
+    if machine and machine.name != machine_name:
+        return jsonify({'response': 'Invalid request.'})
+    sid_list = [item[0] for item in db.query(Training.trainee_id).filter_by(machine_id=machine_id).filter_by(invalidation_date=None)\
+        .filter_by(quiz_score=100.0).all()]
+    access_list = db.query(HawkCard).filter(HawkCard.sid.in_(sid_list)).all()
+    return jsonify({'response': 'Request Successful', 'users':[{'sid': card.sid, 'name': card.user.name, 'facility': card.facility, 'card_number': card.card} for card in access_list]})
