cfec89547653447138ea3d135af255421dfbfc52
Alexander Lukens <alexdlukens@gmail.com>
2021-07-13 14:03:06 -0500

    Refined Training page & buttons

--- safety-quiz/scripts/animations.js
+++ safety-quiz/scripts/animations.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event){
-    // var progressbars = document.getElementsByClassName("progress-bar")
+    var progressbars = document.getElementsByClassName("progress-bar")
     // if(progressbars.length != 0){
     //     var final_progress_values = []
     //     console.log("progressbars.length = ", progressbars.length)
@@ -8,27 +8,27 @@ document.addEventListener("DOMContentLoaded", function(event){
     //             progressbars[i].style.width = "0%";
     //
     //     }
-    //     for(var i=0; i< progressbars.length; i++){
-    //         animate_progress(progressbars[i],final_progress_values[i], i)
-    //     }
-    //
-    //     function animate_progress (target, value, delay_weight){
-    //             anime({
-    //                 targets: target,
-    //                 width: value,
-    //                 easing: 'spring',
-    //                 autoplay: true,
-    //                 delay: 500*delay_weight,
-    //                 round: 1,
-    //                 update: function(){
-    //                     target.innerHTML = "<div style='color: black; font-weight: bold'>" + JSON.stringify((value)) + "</div>"
-    //                 }
-    //             })
-    //     }
-
-        console.log("Progress bars:", progressbars)
-        console.log("final_progress_values:", final_progress_values)
-    }
+        // for(var i=0; i< progressbars.length; i++){
+        //     animate_progress(progressbars[i],final_progress_values[i], i)
+        // }
+        //
+        // function animate_progress (target, value, delay_weight){
+        //         anime({
+        //             targets: target,
+        //             width: value,
+        //             easing: 'spring',
+        //             autoplay: true,
+        //             delay: 500*delay_weight,
+        //             round: 1,
+        //             update: function(){
+        //                 target.innerHTML = "<div style='color: black; font-weight: bold'>" + JSON.stringify((value)) + "</div>"
+        //             }
+        //         })
+        // }
+        //
+        // console.log("Progress bars:", progressbars)
+        // console.log("final_progress_values:", final_progress_values)
+    // }
 
     var first_loop = true;
     anime({
