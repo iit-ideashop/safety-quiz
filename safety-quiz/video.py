250521c3edad2684987167ee086ddc5f67ce5b5c
Alexander Lukens <alexdlukens@gmail.com>
2021-07-16 16:13:03 -0500

    Basic Video links being shown in available trainings. Need to populate machines and video database with all training videos now

--- safety-quiz/video.py
+++ safety-quiz/video.py
@@ -24,8 +24,8 @@ def safety(video_id):
 @video.route('/video', methods=['POST'])
 def return_safety():
     db = g.db_session()
-    print(request.form['sid'])
-    db.add(Training(trainee_id=int(request.form['sid']), trainer_id=20000000, machine_id=9, date=sa.func.now()))
+    print(session['sid'])
+    db.add()
     db.commit()
     flash("Thank you for participating in the Assembly Area Fall 2020 COVID training. Your verification quiz will be available on this site in one week. \
         You can re-watch the video at any time by visiting https://wiki.ideashop.iit.edu/index.php?title=Safety_Training",'success')
