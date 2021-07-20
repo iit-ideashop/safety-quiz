98da1336c39dd92ab972404aa8cb0c3b4fdc5022
Alexander Lukens <alexdlukens@gmail.com>
2021-07-20 15:42:10 -0500

    Overhauled querying, added machineEnabled check in locked_query object. Used json loads/dumps when checking machine parents.

--- safety-quiz/userflow.py
+++ safety-quiz/userflow.py
@@ -6,6 +6,7 @@ import sqlalchemy as sa
 from checkIn.model import User, UserLocation, Type, Training, Machine, Quiz, Question, Option, MissedQuestion, init_db, \
     Major, College, HawkCard
 from flask import Blueprint, current_app
+import json
 
 userflow = Blueprint('userflow', __name__)
 
@@ -50,23 +51,27 @@ def training_interface():
 
 
     # For Locked and available
-    locked_query = db.query(Machine).outerjoin(Training).filter(Training.trainee_id == session['sid']) \
-        .filter(Machine.parent_id != None).all()
-
+    locked_query = db.query(Machine).filter_by(machineEnabled=1).all()
+    temp_parents = None
     completed_machine_ids = list()
     for completed in completed_list:
         completed_machine_ids.append(completed.machine_id)
     for i in locked_query:
-        parents_id = i.parent_id[1:-1].split(",")
+        # print("in locked query: ",i)
+        if(i.parent_id is not None):
+            temp_parents = json.loads(i.parent_id)
         locked_flag = False
-        for j in parents_id:
-            if int(j) not in completed_machine_ids:
-                locked_flag = True
-                break
+        if(temp_parents):
+            for j in temp_parents:
+                if int(j) not in completed_machine_ids:
+                    locked_flag = True
+                    break
         if locked_flag:
             locked_list.append(i)
+            # print("in locked list:", i)
         else:
             available_list.append(i)
+            # print("in available list:", i)
 
     machine_video_ids = Machine.getMachineVideoIds()
 
