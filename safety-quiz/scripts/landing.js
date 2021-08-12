d6c584b5e28cc023ee6275ef2a86425ab11a688b
Alexander Lukens <alexdlukens@gmail.com>
2021-08-12 11:21:56 -0500

    Flip cards finally work correctly

--- safety-quiz/scripts/landing.js
+++ safety-quiz/scripts/landing.js
@@ -1,43 +1,37 @@
 
-
-function showCNC(){
-        var $ = function( id ) { return document.getElementById( id ); };
-        console.log('CNC button Clicked!')
-        if($('cnc-Info').style.getPropertyValue('display') != 'none'){
-            $('cnc-Info').style.setProperty('display', 'none')
-            $('cnc-CTA').style.setProperty('opacity',1)
-            console.log('CNC details shown!')
-        }
-        else{
-            $('cnc-Info').style.setProperty('display', 'block')
-            $('cnc-CTA').style.setProperty('opacity',0)
-            console.log('CNC details hidden!')
-        }
+function flipCard(object) {
+        object.classList.toggle("flipCard")
+        console.log("in handler")
+        setTimeout(function(){ object.classList.toggle("flipCard") }, 25000);
     }
 
-function showBack(id, show){
-        var $ = function( id ) { return document.getElementById( id ); };
-        console.log('LP button Clicked!, show = ',show, 'id = ', id)
-        if (show){
-            id.style.setProperty('transform','rotateY(180deg)')
-        }
-        else{
-            id.style.setProperty('transform','rotateY(0deg)')
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const cnc = document.getElementById('cnc_card')
+    const laser = document.getElementById('laser_card')
+    const printers = document.getElementById('print_card')
 
-        // $('lp-front').style.setProperty('transform', 'rotateY(180deg)')
-    }
+    printers.addEventListener("mousedown", function() {
+        flipCard(printers)
+    })
+    laser.addEventListener("mousedown", function() {
+        flipCard(laser)
+    })
+    cnc.addEventListener("mousedown", function() {
+        flipCard(cnc)
+    })
 
+
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     $(function () {
-      $("#locationPopover").popover({
-          html: true,
-          content: function(){
-              return $("#locationPopoverContent").html();
-          }
-      });
+        $("#locationPopover").popover({
+            html: true,
+            content: function () {
+                return $("#locationPopoverContent").html();
+            }
+        });
     });
-
-    var location = document.getElementById('locationPopover')
 });
+
+
